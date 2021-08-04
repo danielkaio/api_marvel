469027518c34eac42dbb3683902ee71e8a1cd872
daniel <dani.ajala@yahoo.com>
2021-08-04 18:14:37 -0300

    mostrando imagens na tela

--- index.js
+++ index.js
@@ -2,18 +2,33 @@ let publica = "e5aa1403018cf8a6fbf09c90bf032f84"
 let temp = "1627422554572"
 let hash = "007118e30e0d26cec1bb66fdd0a39bc7"
 
-fetch(`http://gateway.marvel.com/v1/public/comics?ts=${temp}&apikey=${publica}&hash=${hash}&limit=6`)
+fetch(`http://gateway.marvel.com/v1/public/comics?ts=${temp}&apikey=${publica}&hash=${hash}&limit=20`)
 
 .then((res) => {
     return res.json()
 })
 
-.then((dados) => {
 
-    console.log(dados)
+.then((heroes) => {
 
+    heroes.data.results.forEach(element => {
+
+        let caminho = element.thumbnail.path + '.' + element.thumbnail.extension;
+
+
+        let personagem = (caminho) => {
+            let img = document.createElement('img')
+            let = div = document.querySelector('#hero').appendChild(img)
+                .setAttribute('src', caminho)
+        }
+
+        personagem(caminho)
+
+    });
 })
 
-.catch((erro) => {
-    console.log("seu erro foi" + erro)
-})
+
+
+
+
+.catch((erro) => console.log("seu erro foi" + erro))
