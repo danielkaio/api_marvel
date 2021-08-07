749b9abc6e45e7a3b2d05a154a11afe91f4c12a1
daniel <dani.ajala@yahoo.com>
2021-08-07 17:05:47 -0300

    responsividade com flex-box

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let publica = "e5aa1403018cf8a6fbf09c90bf032f84"
 let temp = "1627422554572"
 let hash = "007118e30e0d26cec1bb66fdd0a39bc7"
 
-fetch(`http://gateway.marvel.com/v1/public/comics?ts=${temp}&apikey=${publica}&hash=${hash}&limit=20`)
+fetch(`http://gateway.marvel.com/v1/public/characters?ts=${temp}&apikey=${publica}&hash=${hash}&limit=20`)
 
 .then((res) => {
     return res.json()
@@ -10,16 +10,22 @@ fetch(`http://gateway.marvel.com/v1/public/comics?ts=${temp}&apikey=${publica}&h
 
 
 .then((heroes) => {
+    console.log(heroes)
 
     heroes.data.results.forEach(element => {
 
         let caminho = element.thumbnail.path + '.' + element.thumbnail.extension;
+        let texto = element.name
 
 
         let personagem = (caminho) => {
-            let img = document.createElement('img')
+            let img = document.createElement('img');
             let = div = document.querySelector('#hero').appendChild(img)
-                .setAttribute('src', caminho)
+
+
+
+
+            .setAttribute('src', caminho)
         }
 
         personagem(caminho)
