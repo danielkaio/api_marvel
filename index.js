c076749d2a41b22747ce3c8ce243a40c76119d89
daniel <dani.ajala@yahoo.com>
2021-08-11 00:49:20 -0300

    final

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let publica = "e5aa1403018cf8a6fbf09c90bf032f84"
 let temp = "1627422554572"
 let hash = "007118e30e0d26cec1bb66fdd0a39bc7"
 
-fetch(`http://gateway.marvel.com/v1/public/characters?ts=${temp}&apikey=${publica}&hash=${hash}&limit=20`)
+fetch(`http://gateway.marvel.com/v1/public/characters?ts=${temp}&apikey=${publica}&hash=${hash}&limit=10`)
 
 .then((res) => {
     return res.json()
